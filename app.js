aab9db301104ae9ab2e5b213b71cce7f06965d02
Ollie <myself.ollie.lee@gmail.com>
2020-07-12 09:25:59 +0930

    add depency

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var faker = require('faker')
 const { database } = require('faker')
 
 var connection = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    database:'invitation',
-    password:'zxas3229'
+    host: 'localhost',
+    user: 'root',
+    database: 'invitation',
+    password: 'zxas3229'
 })
 
 //query statement 
@@ -14,7 +14,11 @@ var connection = mysql.createConnection({
 var q = 'select count(*) as total from users'
 
 //inserting data
-var person = {email:faker.internet.email()}
+var person = {
+    email: faker.internet.email(),
+    //mysql library help us convert the format
+    created_at: faker.date.past()
+}
 
 //for inserting database, not use this way
 // connection.query(q,function(error, results, fields ){
@@ -22,12 +26,32 @@ var person = {email:faker.internet.email()}
 //     console.log(results[0]);
 // })
 
-connection.query('insert into users set ?', person, function (err, result) {
-    if(err) throw err;
-    console.log(result)
-})
+// connection.query('insert into users set ?', person, function (err, result) {
+//     if(err) throw err;
+//     console.log(result)
+// })
 
-//!after node this file, the connection will begin
-//! need to enter ctrl + c to exit or
+// //!after node this file, the connection will begin
+// //! need to enter ctrl + c to exit or
+
+// connection.end()
+
+//! inserting lots of data
+
+var data = [];
+for (var i = 0; i < 500; i++) {
+    data.push([
+        faker.internet.email(),
+        faker.date.past()
+    ])
+}
+
+var q = 'insert into users (email, created_at) values ?';
+
+connection.query(q, [data], function (err, result) {
+    console.log("result", result)
+    console.log("err", err)
+
+})
 
 connection.end()
